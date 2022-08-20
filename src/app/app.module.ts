83108d3d57f78279c3982e500fd655b3fa4f2978
Rusheins <scarafioccaagustin@gmail.com>
2022-08-20 09:55:29 -0300

    subo index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { NgCircleProgressModule } from 'ng-cirle-progress';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
@@ -17,8 +17,16 @@ import { HysComponent } from './components/hys/hys.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    NgCircleProgressModule.forRoot({})
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
