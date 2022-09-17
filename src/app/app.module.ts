8213465b6627366b46d826237633a676e465288c
Rusheins <scarafioccaagustin@gmail.com>
2022-09-17 00:42:40 -0300

    subo index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { TitulosDeEducacionComponent } from './components/titulos-de-educacion/titulos-de-educacion.component';
@@ -24,15 +23,6 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({
-      // set defaults here
-      radius: 100,
-      outerStrokeWidth: 16,
-      innerStrokeWidth: 8,
-      outerStrokeColor: "#78C000",
-      innerStrokeColor: "#C7E596",
-      animationDuration: 300,
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
